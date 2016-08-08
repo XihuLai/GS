0fb46445b73fdc36a2e95c6f9afaf32a8b550b43
kevin <liwentong@dadalo.cn>
2016-08-08 09:49:18 +0800

    no message

--- src/com/dyz/persist/util/Naizi.java
+++ src/com/dyz/persist/util/Naizi.java
@@ -88,109 +88,126 @@ public class Naizi {
         }
         for(int i=0;i<9;i++){
             if(temp_arr[i]>0){
-                    if (i < 7) {
-                        if(temp_arr[i] >= 2 && isjiang == false && temp_arr[i+1] == 0){//先判断有将牌没有，如果没有将牌，先将这两张牌作将
-                            temp_arr[i] -= 2;
-                            isjiang = true;
-                            i--;
-                        }else {
-                            // 如果 这张牌的下一张和再下一张都不为空的情况。可以组成一级牌
-                            if (temp_arr[i + 1] > 0 && temp_arr[i + 2] > 0) {
+                    if(temp_arr[i] >= 3){
+                        if(i<7) {
+                            if (temp_arr[i + 1] == 1 && temp_arr[i + 2] == 1) {
                                 temp_arr[i]--;
                                 temp_arr[i + 1]--;
                                 temp_arr[i + 2]--;
                                 i--;
-                            } else if (temp_arr[i + 1] > 0 && temp_arr[i + 2] == 0) {
-                                //如果这张牌的下一张不为空，再下一张为空，需要一张赖子
-                                temp_arr[i]--;
-                                temp_arr[i + 1]--;
-                                result++;
+                            } else {
+                                temp_arr[i] -= 3;
                                 i--;
-                            } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] > 0) {
-                                //如果下一张为空，再下一张不为空，先判断有将没有，如果没有将，并且这张牌只有一张，补一张赖子组成将
-                                //                                                             如果这张牌有两张以上，直接做将，不补赖子。
-                                //                                                如果有将的情况，为中间补一张赖子。
-                                if(isOne){
-                                    if (isjiang == false) {
+                            }
+                        }else{
+                            temp_arr[i] -= 3;
+                            i--;
+                        }
+                    }else {
+                        if (i < 7) {
+                            if (temp_arr[i] >= 2 && isjiang == false && temp_arr[i + 1] == 0) {//先判断有将牌没有，如果没有将牌，先将这两张牌作将
+                                temp_arr[i] -= 2;
+                                isjiang = true;
+                                i--;
+                            } else {
+                                // 如果 这张牌的下一张和再下一张都不为空的情况。可以组成一级牌
+                                if (temp_arr[i + 1] > 0 && temp_arr[i + 2] > 0) {
+                                    temp_arr[i]--;
+                                    temp_arr[i + 1]--;
+                                    temp_arr[i + 2]--;
+                                    i--;
+                                } else if (temp_arr[i + 1] > 0 && temp_arr[i + 2] == 0) {
+                                    //如果这张牌的下一张不为空，再下一张为空，需要一张赖子
+                                    temp_arr[i]--;
+                                    temp_arr[i + 1]--;
+                                    result++;
+                                    i--;
+                                } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] > 0) {
+                                    //如果下一张为空，再下一张不为空，先判断有将没有，如果没有将，并且这张牌只有一张，补一张赖子组成将
+                                    //                                                             如果这张牌有两张以上，直接做将，不补赖子。
+                                    //                                                如果有将的情况，为中间补一张赖子。
+                                    if (isOne) {
+                                        if (isjiang == false) {
+                                            if (temp_arr[i] == 1) {
+                                                temp_arr[i] = 0;
+                                                isjiang = true;
+                                                result++;
+                                                i--;
+                                            }
+                                        }
+                                    } else {
+                                        temp_arr[i]--;
+                                        temp_arr[i + 2]--;
+                                        result++;
+                                        i--;
+                                    }
+                                } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] == 0) {
+                                    //如果下一张和再下一张牌都为空的情况，先判断有将没得，如果有将了，为下一张牌补一张赖子
+                                    //                                                    如果还没有将，并且这张牌只有一张，补一张赖子组成将
+                                    //                                                                  如果这张牌有两张以上，直接做将，不补赖子。
+                                    if (isjiang == true) {
+                                        temp_arr[i + 1]++;
+                                        result++;
+                                        i--;
+                                    } else {
                                         if (temp_arr[i] == 1) {
                                             temp_arr[i] = 0;
                                             isjiang = true;
                                             result++;
                                             i--;
-                                        }
-                                    }
-                                }else {
-                                    temp_arr[i]--;
-                                    temp_arr[i + 2]--;
-                                    result++;
-                                    i--;
-                                }
-                            } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] == 0) {
-                                //如果下一张和再下一张牌都为空的情况，先判断有将没得，如果有将了，为下一张牌补一张赖子
-                                //                                                    如果还没有将，并且这张牌只有一张，补一张赖子组成将
-                                //                                                                  如果这张牌有两张以上，直接做将，不补赖子。
-                                if (isjiang == true) {
-                                    temp_arr[i + 1]++;
-                                    result++;
-                                    i--;
-                                } else {
-                                    if (temp_arr[i] == 1) {
-                                        temp_arr[i] = 0;
-                                        isjiang = true;
-                                        result++;
-                                        i--;
-                                    } else {
-                                        if (temp_arr[i] >= 2) {
-                                            isjiang = true;
-                                            temp_arr[i] -= 2;
-                                            i--;
+                                        } else {
+                                            if (temp_arr[i] >= 2) {
+                                                isjiang = true;
+                                                temp_arr[i] -= 2;
+                                                i--;
+                                            }
                                         }
                                     }
                                 }
                             }
-                        }
-                    }else{
-                        //牌面点子大得7的时候，先判断有没有将牌，如果没有，并且这张牌只有一张，补一张赖子组成将
-                        //                                                 如果这张牌有两张以上，直接做将，不补赖子。
-                        //                                      如果有将牌，则判断当前牌和下一张牌是否为空，如果都不为空，在前面补一张赖子
-                        //                                                  如果下一张牌为空，则将当前牌补成坎，缺多少张补多少张赖子
-                        if(i == 7){
-                            if(isjiang == false){
-                                if(temp_arr[i] == 1){
-                                    temp_arr[i] = 0;
-                                    result++;
-                                    isjiang = true;
-                                }else if (temp_arr[i] >= 2){
-                                    temp_arr[i] -=  2;
-                                    isjiang = true;
-                                    i--;
+                        } else {
+                            //牌面点子大得7的时候，先判断有没有将牌，如果没有，并且这张牌只有一张，补一张赖子组成将
+                            //                                                 如果这张牌有两张以上，直接做将，不补赖子。
+                            //                                      如果有将牌，则判断当前牌和下一张牌是否为空，如果都不为空，在前面补一张赖子
+                            //                                                  如果下一张牌为空，则将当前牌补成坎，缺多少张补多少张赖子
+                            if (i == 7) {
+                                if (isjiang == false) {
+                                    if (temp_arr[i] == 1) {
+                                        temp_arr[i] = 0;
+                                        result++;
+                                        isjiang = true;
+                                    } else if (temp_arr[i] >= 2) {
+                                        temp_arr[i] -= 2;
+                                        isjiang = true;
+                                        i--;
+                                    }
+                                } else {
+                                    if (temp_arr[i] > 0 && temp_arr[i + 1] > 0) {
+                                        temp_arr[i]--;
+                                        temp_arr[i + 1]--;
+                                        result++;
+                                        i--;
+                                    } else if (temp_arr[i] > 0 && temp_arr[i + 1] == 0) {
+                                        result = result + 3 - temp_arr[i];
+                                        temp_arr[i] = 0;
+                                    }
                                 }
-                            }else {
-                                if (temp_arr[i] > 0 && temp_arr[i + 1] > 0) {
-                                    temp_arr[i]--;
-                                    temp_arr[i+1]--;
-                                    result++;
-                                    i--;
-                                } else if (temp_arr[i] > 0 && temp_arr[i + 1] == 0) {
+                            } else {
+                                if (isjiang == false) {
+                                    if (temp_arr[i] == 1) {
+                                        temp_arr[i] = 0;
+                                        result++;
+                                        isjiang = true;
+                                    } else if (temp_arr[i] >= 2) {
+                                        temp_arr[i] -= 2;
+                                        isjiang = true;
+                                        i--;
+                                    }
+                                } else {
+                                    //如果当前牌为9点时，直接补成坎
                                     result = result + 3 - temp_arr[i];
                                     temp_arr[i] = 0;
                                 }
-                            }
-                        }else{
-                            if(isjiang == false) {
-                                if(temp_arr[i] == 1){
-                                    temp_arr[i] = 0;
-                                    result++;
-                                    isjiang = true;
-                                }else if (temp_arr[i] >= 2){
-                                    temp_arr[i] -=  2;
-                                    isjiang = true;
-                                    i--;
-                                }
-                            }else{
-                                //如果当前牌为9点时，直接补成坎
-                                result = result + 3 - temp_arr[i];
-                                temp_arr[i] = 0;
                             }
                         }
                     }
@@ -212,45 +229,50 @@ public class Naizi {
         int result = 0;
         for(int i=0;i<9;i++) {
             if(temp_arr[i] > 0) {
-                if (i < 7) {
-                    if (temp_arr[i + 1] > 0 && temp_arr[i + 2] > 0) {
-                        temp_arr[i]--;
-                        temp_arr[i + 1]--;
-                        temp_arr[i + 2]--;
-                        i--;
-                    } else if (temp_arr[i + 1] > 0 && temp_arr[i + 2] == 0) {
-                        temp_arr[i]--;
-                        temp_arr[i + 1]--;
-                        result++;
-                        i--;
-                    } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] > 0) {
-                        temp_arr[i]--;
-                        temp_arr[i + 2]--;
-                        result++;
-                        i--;
-                    } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] == 0) {
-                        if (temp_arr[i] == 2) {
-                            temp_arr[i] = 0;
-                            result++;
-                        } else {
-                            temp_arr[i] = 0;
-                            result  += 2;
-                        }
-                    }
-                } else {
-                    if(i == 7) {
-                        if (temp_arr[i] > 0 && temp_arr[i + 1] > 0) {
+                if(temp_arr[i] >= 3){
+                    temp_arr[i] -= 3;
+                    i--;
+                }else {
+                    if (i < 7) {
+                        if (temp_arr[i + 1] > 0 && temp_arr[i + 2] > 0) {
                             temp_arr[i]--;
-                            temp_arr[i+1]--;
+                            temp_arr[i + 1]--;
+                            temp_arr[i + 2]--;
+                            i--;
+                        } else if (temp_arr[i + 1] > 0 && temp_arr[i + 2] == 0) {
+                            temp_arr[i]--;
+                            temp_arr[i + 1]--;
                             result++;
                             i--;
-                        } else if (temp_arr[i] > 0 && temp_arr[i + 1] == 0) {
+                        } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] > 0) {
+                            temp_arr[i]--;
+                            temp_arr[i + 2]--;
+                            result++;
+                            i--;
+                        } else if (temp_arr[i + 1] == 0 && temp_arr[i + 2] == 0) {
+                            if (temp_arr[i] == 2) {
+                                temp_arr[i] = 0;
+                                result++;
+                            } else {
+                                temp_arr[i] = 0;
+                                result += 2;
+                            }
+                        }
+                    } else {
+                        if (i == 7) {
+                            if (temp_arr[i] > 0 && temp_arr[i + 1] > 0) {
+                                temp_arr[i]--;
+                                temp_arr[i + 1]--;
+                                result++;
+                                i--;
+                            } else if (temp_arr[i] > 0 && temp_arr[i + 1] == 0) {
+                                result = result + 3 - temp_arr[i];
+                                temp_arr[i] = 0;
+                            }
+                        } else {
                             result = result + 3 - temp_arr[i];
                             temp_arr[i] = 0;
                         }
-                    }else{
-                        result  = result + 3-temp_arr[i];
-                        temp_arr[i] = 0;
                     }
                 }
             }
@@ -265,7 +287,7 @@ public class Naizi {
     }
 
     public static void main(String[] args){
-        int [] test = new int[]{0,1,0,0,1,1,1,0,0,     0,0,0,0,0,0,0,0,0,     0,0,1,1,1,0,0,0,0,   0,0,0,0,1,0,0};
+        int [] test = new int[]{0,0,3,0,1,1,0,0,0,     0,0,0,0,0,0,1,1,1,     2,0,0,0,0,0,0,2,0,   0,0,0,0,2,0,0};
         getNeedHunNum(test);
     }
 }
