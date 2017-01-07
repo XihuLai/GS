28947a1589206120032eff36abc04682c6583680
zshadmin@gmail.com <349955417@qq.com>
2017-01-07 15:59:21 +0800

    修改胡法

--- src/com/context/Rule.java
+++ src/com/context/Rule.java
@@ -10,34 +10,42 @@ public class Rule {
 	//转转麻将胡牌，普通胡牌
 	
 	
-	//长沙麻将胡牌
-    public static String Hu_qs_dasixi = "qs_dasixi";
-    public static String Hu_qs_banban = "qs_banban";
-    public static String Hu_qs_queyise = "qs_queyise";
-    public static String Hu_qs_liuliu = "qs_liuliu";
-    public static String Hu_qs_ping = "qs_ping";
+	//胡牌方法
+	  
+    public static String Hu_pengpeng = "pengpeng";//碰碰胡
+    public static String Hu_haohuaqxd = "haohuaqxd";//豪华七小对
+    public static String Hu_qxd = "qxd";//七小对
+    public static String Hu_qingyise = "qingyise";//清一色
+    public static String Hu_hunyise = "hunyise";//混一色
+    public static String Hu_yitiaolong = "yitiaolong";//一条龙
+    public static String Hu_shisanyao = "shisanyao";//十三幺
+    public static String Hu_menqing = "menqing";//门清
+    public static String Hu_quemen = "quemen";//缺门
+    public static String Hu_gouzhang = "gouzhang";//够张
+    public static String Hu_biankandiao = "biankandiao";//边坎钓
+    public static String Hu_kanwuwan = "kanwuwan";//坎五万
+    public static String Hu_caishen = "caishen";//带财神
     
-    public static String Hu_pengpeng = "pengpeng";
-    public static String Hu_jiangjiang = "jiangjiang";
-    public static String Hu_haohuaqxd = "haohuaqxd";
-    public static String Hu_qianggang = "qianggang";
-    public static String Hu_quanqiuren = "quanqiuren";
+    public static String Hu_qianggang = "qianggang";//抢杠胡
+    public static String Hu_quanqiuren = "quanqiuren";//全求人
     
     //公用
 	public static String Hu_zi_common = "zi_common";//自摸普通胡
 	public static String Hu_other_common = "other_common";//别人自摸普通胡
 	public static String Hu_d_self = "d_self";//别人点自己胡
 	public static String Hu_d_other = "d_other";//自己点别人胡
-    public static String Hu_d_qingyise = "qingyise";//点炮清一色
+	
+    public static String Hu_d_qingyise = "other_qingyise";//点炮清一色
     public static String Hu_zi_qingyise = "zi_qingyise";//自摸清一色
-    public static String Hu_haidilao = "haidilao";
-    public static String Hu_haidipao = "haidipao";
-    public static String Hu_self_qixiaodui = "self_qixiaodui";
-    public static String Hu_other_qixiaodui = "other_qixiaodui";
-    public static String Hu_gangshangpao = "gangshangpao";
-    public static String Hu_gangshanghua = "gangshanghua";
-    public static String Hu_tian = "tian";
-    public static String Hu_di = "di";
+    public static String Hu_haidilao = "haidilao";//海底捞
+    public static String Hu_haidipao = "haidipao";//海底炮
+    
+    public static String Hu_self_qixiaodui = "self_qixiaodui";//自己七小对
+    public static String Hu_other_qixiaodui = "other_qixiaodui";//别人七小对
+    public static String Hu_gangshangpao = "gangshangpao";//杠上炮
+    public static String Hu_gangshanghua = "gangshanghua";//杠上花
+    public static String Hu_tian = "tian";//天胡
+    public static String Hu_di = "di";//地胡
     
     //杠牌
     public static String Gang_ming = "ming";
