d00e8c262fb32316d6a7fb6368b9637c6e1dc855
Xihu Lai <westlakeeast@gmail.com>
2017-01-07 23:37:45 +0800

    bug fix

--- src/com/dyz/gameserver/logic/PlayCardsLogic.java
+++ src/com/dyz/gameserver/logic/PlayCardsLogic.java
@@ -2534,9 +2534,13 @@ public class PlayCardsLogic {
 				continue;
 			}
 			pt[0][i]++;
-			rv = checkSevenDouble(pt.clone()) > 0;
+			rv = rv || checkSevenDouble(pt.clone()) > 0;
 			rv = rv || checkThirteen(pt.clone()) == 1;
 			rv = rv || normalHuPai.checkHu(pt);
+
+			if(rv) {
+				break;
+			}
 		}
 		return rv;
 	}
